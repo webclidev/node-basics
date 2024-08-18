15d568baf1087d5a6ad922731d0adcf493723c28
webclidev <webclidev@gmail.com>
2024-08-18 12:38:17 +0530

    add simple router

--- server.js
+++ server.js
@@ -4,8 +4,24 @@ loadEnv();
 
 const server = http.createServer((req, res) => {
   //   res.setHeader("Content-Type", "text/plain");
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Om Namah Shivay</h1>");
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("<h1>Om Namah Shivay</h1>");
+      break;
+
+    case "/about":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(
+        "<h1>Shiva</h1><p>Is the Supreme Being in Shaivism, one of the major traditions within Hinduism. Shiva is known as The Destroyer within the Trimurti, the Hindu trinity which also includes Brahma and Vishnu. In the Shaivite tradition, Shiva is the Supreme Lord who creates, protects and transforms the universe.</p>"
+      );
+      break;
+
+    default:
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<p>Not found</p>");
+      break;
+  }
 });
 
 server.listen(process.env.PORT, () => {
