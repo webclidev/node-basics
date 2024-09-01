80b154673e87cfb1bfdf9df5de5e843ec7a4747f
webclidev <webclidev@gmail.com>
2024-09-01 15:16:39 +0530

    simple apiServer

--- server.js
+++ server.js
@@ -32,6 +32,37 @@ const server = http.createServer(async (req, res) => {
   res.end(data);
 });
 
-server.listen(process.env.PORT, () => {
+const apiServer = http.createServer((req, res) => {
+  const users = [
+    { id: 1, name: "Shiv" },
+    { id: 2, name: "Parvati" },
+    { id: 3, name: "Kartikeya" },
+    { id: 4, name: "Ganesh" },
+  ];
+
+  if (req.url === "/api/users" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(users));
+    res.end();
+  } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+    const id = req.url.split("/")[3];
+    const user = users.find((user) => user.id === parseInt(id));
+    res.setHeader("Content-Type", "application/json");
+    if (user) {
+      res.write(JSON.stringify(user));
+    } else {
+      res.statusCode = 404;
+      res.write(JSON.stringify({ message: "Not found!" }));
+    }
+    res.end();
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 404;
+    res.write(JSON.stringify({ message: "Not found!" }));
+    res.end();
+  }
+});
+
+apiServer.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
 });
