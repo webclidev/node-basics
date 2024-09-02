3008322c28cc15cedf397d90b75582e8572cb17c
webclidev <webclidev@gmail.com>
2024-09-03 01:00:17 +0530

    os module

---
+++ osDemo.js
@@ -0,0 +1,7 @@
+import os from "os";
+console.log("::os.userInfo()::", os.userInfo());
+console.log("::os.totalmem()::", os.totalmem());
+console.log("::os.freemem()::", os.freemem());
+console.log("::os.arch()::", os.arch());
+console.log("::os.cpus()::", os.cpus());
+console.log("::os.type()::", os.type());
